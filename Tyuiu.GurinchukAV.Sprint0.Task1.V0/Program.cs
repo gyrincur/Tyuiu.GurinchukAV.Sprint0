e03ce67977bf5bc67713693829c66b5d0825791e
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-09-12 23:37:42 +0300

    Task1

--- Tyuiu.GurinchukAV.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.GurinchukAV.Sprint0.Task1.V0/Program.cs
@@ -11,8 +11,8 @@ namespace Tyuiu.GurinchukAV.Sprint0.Task1.V0
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World");
-            string name = "Игорь";
-            int age = 45;
+            string name = "Настя";
+            int age = 17;
             Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
